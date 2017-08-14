5e1aa79a49473a9b0b1393f7935dbe8e4ec7b34d
Kitson <kitsonlow99@gmail.com>
2017-08-14 14:18:30 +0800

    Added Database

--- NYP_Framework/Common/Source/BaseDatabase.cpp
+++ NYP_Framework/Common/Source/BaseDatabase.cpp
@@ -1,8 +1,9 @@
 #include "BaseDatabase.h"
+#include "Loader.h"
 
 template <class Object>
-BaseDatabase<Object>::BaseDatabase(const std::string& name, const std::string& filePath, const std::string& tableName)
-    :k_name(name),k_filePath(filePath),k_tableName(tableName)
+BaseDatabase<Object>::BaseDatabase(const std::string& name, const std::string& filePath)
+    :k_name(name),k_filePath(filePath)
 {
 
 }
@@ -16,11 +17,17 @@ BaseDatabase<Object>::~BaseDatabase()
 template <class Object>
 void BaseDatabase<Object>::init()
 {
-    
+	Loader::GetInstance()->LoadData(this->k_filePath);
+	std::vector<std::string> data = Loader::GetInstance()->GetData();
+	int i = 1;
+	for (std::vector<std::string>::iterator it = data.begin();it != data.end();++it)
+	{
+		this->m_database[i] = new Object(*i);
+	}
 }
 
 template <class Object>
 Object* BaseDatabase<Object>::getObject(int id)
 {
-
+	return this->m_database[id];
 }
--- NYP_Framework/Common/Source/BaseDatabase.h
+++ NYP_Framework/Common/Source/BaseDatabase.h
@@ -8,10 +8,10 @@ template <class Object>
 class BaseDatabase
 {
 protected:
-	const std::string k_name, k_filePath, k_tableName;
+	const std::string k_name, k_filePath;
     std::map<int, Object*> m_database;
 public:
-	BaseDatabase(const std::string& name,const std::string& filePath,const std::string& tableName);
+	BaseDatabase(const std::string& name,const std::string& filePath);
 	~BaseDatabase();
 	void init();
 	Object* getObject(int id);
